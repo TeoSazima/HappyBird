4c5a788a9100978585acb5f37c97dd5b49da2b24
TeoSazima <teo.sazima@gmail.com>
2026-09-07 08:34:49 +0200

    - Commmiting other files

--- HappyBird/MainWindow.xaml.cs
+++ HappyBird/MainWindow.xaml.cs
@@ -24,7 +24,10 @@ namespace HappyBird
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        // CONSTANTS
+        private const int pipeWidthInPixels = 30;
+        private const int opieceOfPipeLenghtInPixels = 38;
+        
 
         public bool paused = true;
         public bool gameStarted = false;
@@ -35,25 +38,92 @@ namespace HappyBird
         public bool stillalive = true;
 
         //MAX 10  BIGGEST
-        //MIN 358 LOWEST
+        //MIN 385 LOWEST
         //MID 185
         int height = 185;
         
-        DispatcherTimer timer = new DispatcherTimer();
-        
+        DispatcherTimer renderTimer = new DispatcherTimer();
+        DispatcherTimer newPipeGenerate = new DispatcherTimer();
+
 
 
         public MainWindow()
         {
-            
+
             InitializeComponent();
+
+            renderTimer.Interval = TimeSpan.FromMilliseconds(10);
+            renderTimer.Tick += MainLogic;
+
+            newPipeGenerate.Interval = TimeSpan.FromSeconds(5);
+            newPipeGenerate.Tick += NewPipe;
             
-            timer.Interval = TimeSpan.FromMilliseconds(10);
-            timer.Tick += MainLogic;
-            timer.Start();
+
+
 
         }
 
+        private void NewPipe(object sender, EventArgs e)
+        {
+            // 1 ELEMENT JE 23px
+            // CELKEM 230px = VELIKOST OBRAZOVKY
+            char[] hitboxArray = new char[10];
+            
+            Random rand = new Random();
+            int x = rand.Next(0, hitboxArray.Length);
+            
+            
+                if (x == 0)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        hitboxArray[x+j] = 'X';
+                    }
+                }
+                else if (x == 9)
+                {
+                    for (int j = -1; j < 1; j++)
+                    {
+                        hitboxArray[x+j] = 'X';
+                    }
+                }
+                else
+                {
+                    for (int j = -1; j < 2; j++)
+                    {
+                        hitboxArray[x+j] = 'X';
+                    }
+                }
+            
+            
+            
+            int FirstPipeLenght = 0;
+            
+            for (int i = 0; i < hitboxArray.Length; i++)
+            {
+
+                if (hitboxArray[i] == 'X' && FirstPipeLenght != 0) break;
+
+                FirstPipeLenght++;
+            }
+            
+            var pipeTop =  new Rectangle();
+            pipeTop.Height = FirstPipeLenght * opieceOfPipeLenghtInPixels;
+            pipeTop.Width = pipeWidthInPixels;
+            pipeTop.Fill = Brushes.Chartreuse;
+            pipeTop.HorizontalAlignment = HorizontalAlignment.Center;
+            pipeTop.Margin = new Thickness(0,10,0,0);
+            pipeTop.Stroke  = Brushes.Black;
+            pipeTop.VerticalAlignment = VerticalAlignment.Top;
+            
+            grid.Children.Add(pipeTop);
+            
+            
+            
+            
+            
+        }
+        
         public void MainLogic(object sender, EventArgs e)
         {
                 // KONTROLA ZDA JE LEVE TLACITKO MYSI ZMACKNUTE
@@ -92,6 +162,9 @@ namespace HappyBird
                 paused = false;
                 gameStarted = true;
                 txtblk_StartText.Visibility = Visibility.Hidden;
+                
+                renderTimer.Start();
+                newPipeGenerate.Start();
             }
         }
 
@@ -103,21 +176,18 @@ namespace HappyBird
                 btn_Resume.Content = "▶︎";
                 paused = !paused;
                 
-                //SPUSTI HLAVNI LOGIKU
-                //MainLogic();
-                   
-                
-
+                renderTimer.Start();
+                newPipeGenerate.Start();
             }
             else
             {
                 btn_Resume.Content = "⬛";
                 paused = !paused;
+                
+                renderTimer.Stop();
+                newPipeGenerate.Stop();
             }
         }
-
-
-        
     }
 }
 
